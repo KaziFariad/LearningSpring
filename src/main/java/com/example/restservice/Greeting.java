57ea31a297ce31526b696458337fc70e0aae3e2e
Kazi Fariad <4kfislam@gmail.com>
2021-12-12 18:34:47 +0530

    Initial commit

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,27 @@
+package com.example.restservice;
+
+public class Greeting {
+    private final Long id;
+    private final String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
